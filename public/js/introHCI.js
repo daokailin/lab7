3304eceedac556d7d47b6c251c374fff2521148d
Daokai Lin <dalin@ucsd.edu>
2020-02-15 15:35:30 -0800

    added lab7 changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,12 +2,20 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+    // your code here\
+    $("#btnBig").click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+    ga('create', 'UA-158628618-1', 'auto');
+    ga('send', 'event', 'like', 'click');
+
+
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var projects = require('../projects.json');
+var json = require('../projects.json');
 
 /*
  * GET home page.
  */
 
-exports.view = function(request, response){
-  	response.render('index', projects);
+exports.view = function(request, response) {
+    response.render('index', json);
+    json["view"] = false;
+
+};
+
+exports.viewAlt = function(request, response) {
+    let { projects } = json;
+    for (let i = 0; i < projects.length; i++) {
+        projects[i]['viewAlt'] = true;
+    }
+    response.render('index', json);
 };
